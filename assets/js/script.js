46f2bf6341cdde3a25541cc479c912af60c1e50e
Kaysheridan <kaysheridan1@hotmail.com>
2022-03-03 21:24:18 +0000

    Add functions to move to random circles and the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,25 @@ const circles = document.querySelectorAll('.circle');
 const piggy = document.querySelector('.piggy');
 const timeRemaining = document.querySelector('.time');
 const scores = document.getElementById('score');
+
+// relevant to the score as we need it to always start at 0/
 let result = 0 
 
-let randomCircle = circles[Math.floor(Math.random() * 9)];
-console.log(randomCircle);
+function randomCircle() {
+    circles.forEach(circle => {
+        circle.classList.remove('piggy');
+    }) 
+
+    let randomCircle = circles[Math.floor(Math.random() * 9)];
+randomCircle.classList.add('piggy');
+}
+
+function placePiggy() {
+    let timer = null;
+    timer = setInterval(randomCircle, 600);
+}
+
+placePiggy();
 
 
 
