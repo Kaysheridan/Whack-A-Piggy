c997fe4a6307982afe500a44a1a4c8437bf175a2
Kaysheridan <kaysheridan1@hotmail.com>
2022-03-04 16:38:51 +0000

    Add background pic to Piggy ID

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ const scores = document.getElementById('score');
 // relevant to the score as we need it to always start at 0/
 let result = 0 
 
+//function to remove piggy to blank slate and then place the piggy in a random circle/
 function randomCircle() {
     circles.forEach(circle => {
         circle.classList.remove('piggy');
@@ -16,6 +17,7 @@ function randomCircle() {
 randomCircle.classList.add('piggy');
 }
 
+//sets timer to move piggy every 600 miliseconds/ 
 function placePiggy() {
     let timer = null;
     timer = setInterval(randomCircle, 600);
